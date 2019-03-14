ca533ec9c5b35f99abba213cea848c2bb0516385
Tilak <Tilak@LAPTOP-PPPJKIR7>
2019-03-14 20:31:57 +0530

    Hi bro

---
+++ chill/src/chill/Chill.java
@@ -0,0 +1,8 @@
+package chill;
+
+public class Chill {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
---
+++ chill/src/chill/HI.java
@@ -0,0 +1,14 @@
+package chill;
+
+import org.testng.annotations.Test;
+
+public class HI {
+@Test
+public void test() {
+	System.out.println("Bye");
+}
+@Test
+public void test1() {
+	System.out.println("Hi");
+}
+}
